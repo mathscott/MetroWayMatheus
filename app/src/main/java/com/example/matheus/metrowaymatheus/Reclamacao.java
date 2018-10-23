39cca09db993c06ad1e9cd8874df5547c8a65231
Matheus <matheus.furlan3@gmail.com>
2018-10-23 00:55:44 -0300

    classe reclamacao criada

---
+++ app/src/main/java/com/example/matheus/metrowaymatheus/Reclamacao.java
@@ -0,0 +1,43 @@
+package com.example.matheus.metrowaymatheus;
+
+import java.util.Date;
+
+public class Reclamacao {
+
+    private String estacao;
+    private String nome;
+    private Date data;
+    private String descricao;
+
+    public String getEstacao() {
+        return estacao;
+    }
+
+    public void setEstacao(String estacao) {
+        this.estacao = estacao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
